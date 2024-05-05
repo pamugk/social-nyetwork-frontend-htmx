08648060ee7e52eaa1e80f09d7d4b9f6f00dde9b
Alexander Permyakov <pamugk@gmail.com>
2024-05-05 14:15:26 +0500

    Registration
    - added stub options for regional settings,
    - started service integration

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-    "html/template"
-    "net/http"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-    templates := template.Must(template.ParseFiles("templates/register.html"))
-    http.HandleFunc("/register", func (w http.ResponseWriter, r *http.Request) {
-        templates.ExecuteTemplate(w, "register.html", nil)
-    })
+	templates := template.Must(template.ParseFiles("templates/register.html"))
+	http.HandleFunc("/register", func (w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		templates.ExecuteTemplate(w, "register.html", nil)
+	} else if r.Method == http.MethodPost {
+		_, err := http.Post(os.Getenv("SERVICE_URL"))
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+	})
 
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8081", nil)
 } 
